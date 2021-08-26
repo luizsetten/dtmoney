4ee50f3b06747a20a7f76075e19b94f1a3e4a442
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-08-25 21:58:59 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import { GLobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <>
+      <Header />
       <GLobalStyle />
-    </div>
+    </>
   );
 }
